46d534466e9be1ead1783fceaba7f35e74f24a5a
10VIBU <76595850+10VIBU@users.noreply.github.com>
2021-04-12 14:16:16 -0500

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,10 @@
+﻿let handler = function (m) {
+  // this.sendContact(m.chat, '51967937978', 'Nurutomo', m)
+  this.sendContact(m.chat, '51967937978', 'Vibu OFC', m)
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
---
+++ plugins/samu.js
@@ -0,0 +1,8 @@
+﻿let handler = async m => m.reply(AGRADEZCO A MI HERMANO SAMU POR LOS COMANDOS, UTILIZA ESTE BOT ADECUADAMENTE Y POR FAVOR NO LO SATURES P CTM :v ATT. VIBU OFC
+                                        
+`.trim()) 
+handler.help = ['samu']
+handler.tags = ['info']
+handler.command = /^samu$/i
+
+module.exports = handler
